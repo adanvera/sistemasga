fc78387d02ca9c5a73f166f83a2d312deba71251
Adán Vera <gustavoadanvera@gmail.com>
2022-02-19 16:07:49 -0300

    correcciones de estilos de login form

--- src/componentes/LoginForm.js
+++ src/componentes/LoginForm.js
@@ -14,25 +14,28 @@ function LoginForm({Login, error}) {
         <form onSubmit={submittrigger}>
           <div className='row'>
             <div className='col-md-6' id='background-login'></div>
+
             <div className='col-md-6' id='login-box'>
-              <div id='logo-login'></div>
-              <div>
-                <h4 id='login-text'>GESTIONA TUS PROYECTOS
-                  DE MANERA FÁCIL
-                  Y RÁPIDA
-                </h4>
-              </div>
-              <h1>Iniciar Sesion</h1>
-              <div className='login-box'>
-                <label for="">Nombre de usuario: </label>
-                <input type="text" name="name" id='name'/>
-              </div>
-              <div>
-                <label for="password">Contraseña: </label>
-                <input type="password" name="password" value=""/>
-              </div>
-              
-              <input type="submit" name="login" value="Iniciar sesion"/>
+              <div className='container-fluid h-100'>
+                <div id='logo-login'></div>
+                <div>
+                  <h4 id='login-text'>GESTIONA TUS PROYECTOS DE MANERA FÁCIL Y RÁPIDA</h4>
+                </div>
+                <h1>INICIAR SESION</h1>
+                <div className=''>
+                    <div className='col-md-12 box-input pb-5'>
+                      <label for="">Nombre de usuario: </label>
+                      <input className='input-ct' type="text" name="name" id='name'/>
+                    </div>
+                    <div className='col-md-12 box-input pb-5'>
+                      <label for="password">Contraseña: </label>
+                      <input className='input-ct' type="password" name="password"/>
+                    </div>
+                    <p className='p-0 m-0'>No tienes una cuenta?, <span> crear cuenta</span> </p>
+                    <p  className='p-0 m-0'>Olvidaste tu contraseña?, <span>resetear contraseña</span> </p>
+                    <button className='mt-4 btn-log' type="submit" name="loginbtn">Iniciar sesión</button>
+                  </div>
+                </div>
             </div>
           </div>
         </form>
