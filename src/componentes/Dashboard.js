d1c1ef39b64b25f3e5680287dd374f332fb7d766
Adán Vera <gustavoadanvera@gmail.com>
2022-03-06 11:40:51 -0300

    se agregan secciones de dashboard principal, con datos en duro

--- src/componentes/Dashboard.js
+++ src/componentes/Dashboard.js
@@ -1,9 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {Navbar,Nav,NavItem,NavDropdown,MenuItem,Container,} from 'react-bootstrap'
+import {Navbar,Nav,NavItem,NavDropdown,MenuItem,Container, Form,FormControl, Button} from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { DataContext } from '../context/DataContext'
 import myLogo from '../images/iconwhite.png'
 import myphoto from '../images/perfil.png'
+
+
+
+
+import user_one from '../images/users/user_one.png'
+import user_two from '../images/users/user_two.png'
+import user_three from '../images/users/user_three.png'
+import user_four from '../images/users/user_four.png'
 
 export const Dashboard = () => {
 	const {user} = useContext(DataContext)
@@ -41,25 +49,81 @@ export const Dashboard = () => {
 						<span className="bt-cerrar" onClick={logout}>Cerrar sesion <ion-icon name="power" className="pt-1"></ion-icon></span>
 					</div>
 				</div>
-				<div className="navbar justify-content-between w-100 p-0" variant="dark">
-					<Nav className="items">
-						<ion-icon name="reorder-three-outline"></ion-icon>
-						<Nav.Link href="#home">Inicio</Nav.Link>
-						<Nav.Link href="#features">Proyectos</Nav.Link>
-						<Nav.Link href="#pricing">Item 3</Nav.Link>
-					</Nav>
-					<Nav className="">
-						<Nav.Link href="#pricing" className="d-flex">
-							<div className="t-a">
-								<p className="p-style fw-100">Bienvenido</p>
-								<span className='username'>{nombre}</span>
+				<Container  fluid={true} id="dash">
+					<div className="navbar justify-content-between w-100 p-0" variant="dark">
+						<Nav className="items">
+							<ion-icon name="reorder-three-outline"></ion-icon>
+							<Nav.Link href="#home">Inicio</Nav.Link>
+							<Nav.Link href="#features">Proyectos</Nav.Link>
+							<Nav.Link href="#pricing">Item 3</Nav.Link>
+						</Nav>
+						<Nav className="">
+							<Nav.Link href="#pricing" className="d-flex">
+								<div className="t-a">
+									<p className="p-style fw-100">Bienvenido</p>
+									<span className='username'>{nombre}</span>
+								</div>
+								<div className="pl">
+									<img src={myphoto} alt="" />
+								</div>
+							</Nav.Link>
+						</Nav>
+					</div>
+					<Container fluid={true} id="headdash" className='mt-3'>
+						<div className='row o-t'>
+							<span>Tablero</span>
+							<h1>Nombre del proyecto</h1>
+						</div>
+						<div className='row box-dashboard-head p5co'>
+							<div className='col box-users d-flex'>
+								<div className='user'>
+									<img src={user_one} alt="" />
+								</div>
+								<div className='user'>
+									<img src={user_two} alt="" />
+								</div>
+								<div className='user'>
+									<img src={user_three} alt="" />
+								</div>
+								<div className='user'>
+									<img src={user_four} alt="" />
+								</div>
 							</div>
-							<div className="pl">
-								<img src={myphoto} alt="" />
+							<div className='col filters d-flex'>
+								<div className='col-md-4 pr-1'>
+									<Form.Select aria-label="Filtrar por">
+										<option>Selccionar filtro</option>
+										<option value="1">One</option>
+										<option value="2">Two</option>
+										<option value="3">Three</option>
+									</Form.Select>
+								</div>
+								<div className='col-md-4'>
+									<Form.Select aria-label="Tipo">
+										<option>Selccionar filtro</option>
+										<option value="1">One</option>
+										<option value="2">Two</option>
+										<option value="3">Three</option>
+									</Form.Select>
+								</div>
 							</div>
-						</Nav.Link>
-					</Nav>
-				</div>
+							<div className='col form-search'>
+								<Form className="d-flex">
+									<FormControl
+										type="search"
+										placeholder="Buscar"
+										className="me-2"
+										aria-label="Search"
+									/>
+									<Button variant="outline-success"><ion-icon name="search-outline"></ion-icon></Button>
+								</Form>
+							</div>
+						</div>
+					</Container>
+					<Container fluid={true}>
+
+					</Container>
+				</Container>
 			</Container>
 		</>
 	)
