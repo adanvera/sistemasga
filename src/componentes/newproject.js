65e1312e6637b68f02f4a0884d4661b0e508dc03
Adán Vera <gustavoadanvera@gmail.com>
2022-03-06 19:04:35 -0300

    se crea modulo de crear project

---
+++ src/componentes/newproject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function newproject() {
+  return (
+    <Container fluid={true}>
+
+    </Container>
+  );
+}
+
+export default newproject;
