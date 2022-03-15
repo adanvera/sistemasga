a49d0a50325fb83bd93fc85065e0086039ce5f35
Adán Vera <gustavoadanvera@gmail.com>
2022-03-15 08:59:16 -0300

    se agrega seleccionar rol

--- src/modals/RegisterUser.js
+++ src/modals/RegisterUser.js
@@ -144,6 +144,14 @@ function RegisterUser() {
                     />
                   </FloatingLabel>
                 </div>
+                <div className='col-md-12 mt-3'>
+										<Form.Select aria-label="Tipo">
+											<option>Selccionar rol</option>
+											<option value="1">One</option>
+											<option value="2">Two</option>
+											<option value="3">Three</option>
+										</Form.Select>
+									</div>
               </div>
               <div className="row mt-5 justify-content-center">
                 <div className="col-md-12 bt-centrar">
