0b20d35813029a421ee3499719626ff9af93c72f
Adán Vera <64652682+adanvera@users.noreply.github.com>
2022-03-14 23:13:22 -0300

    se añade modal para resgistrar un usuario

--- src/componentes/Seguridad.js
+++ src/componentes/Seguridad.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import { Container, Button, Modal} from 'react-bootstrap';
+import { useState } from 'react';
+import RegisterUser from '../modals/RegisterUser';
+
 
 function Seguridad() {
+  // const showalert=()=>{
+  //   swal({
+  //     title: "Epaa, esa función está en desarrollo",
+  //     text: "En la proxima ya estará funcionando crack ;) !",
+  //     icon: "warning",
+  //     button: "ok",
+  //   });
+  // }
+
+  function ModalRegister() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="btn btn-cr-pro" onClick={handleShow}>crear usuario</Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Registrar usuario</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <RegisterUser/>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
+  
+  
+  
   return (
-    <h1>Modulo de seguridad</h1>
+    <>
+      <Container fluid={true} className="mt-5" id='seguridad'>
+        <div className='row'>
+          <div className='col pb-2'>
+            <h4>SEGURIDAD</h4>
+          </div>
+          <div className='col a-end'>
+            <div><ModalRegister /></div>
+          </div>
+        </div>
+      </Container> 
+    </>
   );
 }
 
