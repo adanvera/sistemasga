a9128b0c74f57275a79ca99ee18a68ca30d64d24
juanmarcelo5 <marcecinco@htomail.com>
2022-03-20 20:50:18 -0300

    Agreganod roles

--- src/backend/controllers/usuarios.js
+++ src/backend/controllers/usuarios.js
@@ -1,81 +1,73 @@
-const  esRolValido  = require('../helpers/validarRol')
+const { obtenerNombreRole } = require('../helpers/helperNombreRol.js')
+const esRolValido = require('../helpers/validarRol')
 const Usuario = require('../Models/usuario')
-const usuarioPost = async (req,res)=>{
- 
-  
-  const user = new Usuario(req.body)
-  
+const usuarioPost = async (req, res) => {
 
-  try {
-    await user.save()
-    res.status(200).json({
-      msg:'Usuario almacenado correctamente',
-      user
-    })
-  } catch (error) {
-    if(error.code === 11000){
-      return res.status(400).json({
-      msg:'Correo ya registrado'
-    })}
-    console.log(error);
-    return res.status(400).json({
-      msg:'Ocurrio un error inesperado',
-      error
-    })
-    
-  
-  }
-  
-  
-}
-const usuarioModificar = async (req,res)=>{
-  res.json({
-    msg:'mensaje'
-  })
-}
-const usuarioMostrar = async (req,res)=>{
-  try {
-    const usuarios = await Usuario.find({estado:true})
-    res.status(200).json({
-      usuarios
-    })
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({
-      msg:'Ocurrio un error'
-    })
-  }
+	const user = new Usuario(req.body)
 
+	try {
+		await user.save()
+		res.status(200).json({
+			msg: 'Usuario almacenado correctamente',
+			user,
+		})
+	} catch (error) {
+		if (error.code === 11000) {
+			return res.status(400).json({
+				msg: 'Correo ya registrado',
+			})
+		}
+		console.log(error)
+		return res.status(400).json({
+			msg: 'Ocurrio un error inesperado',
+			error,
+		})
+	}
+}
+const usuarioModificar = async (req, res) => {
+	res.json({
+		msg: 'mensaje',
+	})
+}
+const usuarioMostrar = async (req, res) => {
+	try {
+		const usuarios = await Usuario.find({ estado: true })
+		
+		
+		usuarios.rol = res.status(200).json({
+			usuarios,
+		})
+	} catch (error) {
+		console.log(error)
+		return res.status(400).json({
+			msg: 'Ocurrio un error',
+		})
+	}
 }
 
-const usuarioEliminar = async (req,res)=>{
-  console.log(req);
-  const {id} = req.params;
-  console.log(id);
+const usuarioEliminar = async (req, res) => {
+	console.log(req)
+	const { id } = req.params
+	console.log(id)
 
-  const usuario  = await Usuario.findByIdAndUpdate(id,{estado: false});
-  //const usuarioAutenticado = req.usuario;
+	const usuario = await Usuario.findByIdAndUpdate(id, { estado: false })
+	//const usuarioAutenticado = req.usuario;
 
-  
-try {
-  res.json({
-    msg:'Usuario eliminado correctamente',
-      usuario
-  });
-  
-} catch (error) {
-  return res.status(400).json({
-    msg:'Ocurrio un error inesperado',
-    error
-  })
-  
-}
-  
-  
+	try {
+		res.json({
+			msg: 'Usuario eliminado correctamente',
+			usuario,
+		})
+	} catch (error) {
+		return res.status(400).json({
+			msg: 'Ocurrio un error inesperado',
+			error,
+		})
+	}
 }
 module.exports = {
-  usuarioPost,
-  usuarioMostrar,
-  usuarioModificar,
-  usuarioEliminar
+	usuarioPost,
+	usuarioMostrar,
+	usuarioModificar,
+	usuarioEliminar,
 }
