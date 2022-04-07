4bef919f6ff2566f3f045b96251032b7316ba33a
Adán Vera <64652682+adanvera@users.noreply.github.com>
2022-04-07 01:43:51 -0400

    push anterior.. se agrega funcion para habilitar opciones de crear usuarios roles segun el rol que se tenga..  push actual: se agrega modal de edicion de usuarios. se agregan estilos correspondientes

--- src/backend/controllers/usuarios.js
+++ src/backend/controllers/usuarios.js
@@ -1,84 +1,75 @@
-const esRolValido = require('../helpers/validarRol')
-const Usuario = require('../Models/usuario')
-const usuarioPost = async (req, res) => {
+const esRolValido = require("../helpers/validarRol");
+const Usuario = require("../Models/usuario");
 
-	const user = new Usuario(req.body)
+const usuarioPost = async (req, res) => {
+	const user = new Usuario(req.body);
 
 	try {
-		await user.save()
+		await user.save();
 		res.status(200).json({
-			msg: 'Usuario almacenado correctamente',
+			msg: "Usuario almacenado correctamente",
 			user,
-		})
+		});
 	} catch (error) {
 		if (error.code === 11000) {
 			return res.status(400).json({
-				msg: 'Correo ya registrado',
-			})
+				msg: "Correo ya registrado",
+			});
 		}
-		console.log(error)
+		console.log(error);
 		return res.status(400).json({
-			msg: 'Ocurrio un error inesperado',
+			msg: "Ocurrio un error inesperado",
 			error,
-		})
+		});
 	}
-}
+};
+
 const usuariosPut = async (req, res = response) => {
-
-  const { id } = req.params;
-  const {_id,password,...resto} = req.body
-
-  
-
-      const usuario = await Usuario.findByIdAndUpdate(id, resto);
-
-
-
-  res.json({
-      msg: 'put API - usuariosPut',
-      usuario
-  });
-}
-
+	const { id } = req.params;
+	const { _id, password, ...resto } = req.body;
+	const usuario = await Usuario.findByIdAndUpdate(id, resto);
+	res.json({
+		msg: "put API - usuariosPut",
+		usuario,
+	});
+};
 
 const usuarioMostrar = async (req, res) => {
 	try {
-		const usuarios = await Usuario.find({ estado: true })
-		
-		
+		const usuarios = await Usuario.find({ estado: true });
 		usuarios.rol = res.status(200).json({
 			usuarios,
-		})
+		});
 	} catch (error) {
-		console.log(error)
+		console.log(error);
 		return res.status(400).json({
-			msg: 'Ocurrio un error',
-		})
+			msg: "Ocurrio un error",
+		});
 	}
-}
+};
 
 const usuarioEliminar = async (req, res) => {
-	const { id } = req.params
-	console.log(id)
+	const { id } = req.params;
+	console.log(id);
 
-	const usuario = await Usuario.findByIdAndUpdate(id, { estado: false })
+	const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
 	//const usuarioAutenticado = req.usuario;
 
 	try {
 		res.json({
-			msg: 'Usuario eliminado correctamente',
+			msg: "Usuario eliminado correctamente",
 			usuario,
-		})
+		});
 	} catch (error) {
 		return res.status(400).json({
-			msg: 'Ocurrio un error inesperado',
+			msg: "Ocurrio un error inesperado",
 			error,
-		})
+		});
 	}
-}
+};
 module.exports = {
 	usuarioPost,
 	usuarioMostrar,
 	usuariosPut,
 	usuarioEliminar,
-}
+};
--- src/componentes/Tbody.js
+++ src/componentes/Tbody.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import {CButton,CModal,CModalBody,CModalFooter,CModalHeader,CModalTitle} from "@coreui/react";
+import React, { useState } from "react";
+import { FloatingLabel, Form } from "react-bootstrap";
+import { log } from "react-modal/lib/helpers/ariaAppHider";
 import swal from "sweetalert";
-const urlUsers = "http://localhost:4000/api/usuario/"
 
+const urlUsers = "http://localhost:4000/api/usuario/";
 
 export const Tbody = ({ usuario, index }) => {
+
+	const [nombre, setNombre] = useState("");
+	const [apellido, setApellido] = useState("");
+	const [correo, setCorreo] = useState("");
+	const [password, setPassword] = useState("");
+	const [repetirPassword, setRepetirPassword] = useState("");
+	const [rol, setRolUser] = useState("");
+
+
+	const initialState = {
+		nombre: usuario.nombre,
+		apellido: usuario.apellido,
+		correo: usuario.correo,
+		password: usuario.password,
+		repetirPassword: usuario.repetirPassword,
+		rol: usuario.role
+	}
 	const deleteUser = async () => {
 		swal({
 			title: "¿Estas seguro?",
@@ -12,20 +31,31 @@ export const Tbody = ({ usuario, index }) => {
 			icon: "warning",
 			buttons: true,
 			dangerMode: true,
+		}).then(async (willDelete) => {
+			if (willDelete) {
+				swal("Usuario eliminado exitosamente", {
+					icon: "success",
+				});
+				await fetch(urlUsers + usuario.uui, {
+					method: "DELETE",
+					headers: { "Content-Type": "application/json" },
+				});
+			}
+		});
+	};
+
+	const [visible, setVisible] = useState(false);
+
+	const submitToggle = async (e) => {
+		e.preventDefault();
+
+		swal({
+			title:"hola",
+			icon:"success"
 		})
-			.then(async (willDelete) => {
-				if (willDelete) {
-					swal("Usuario eliminado exitosamente", {
-						icon: "success",
-					});
-					await fetch(urlUsers + usuario.uui, { method: 'DELETE', headers: { "Content-Type": "application/json" } })
-				}
-			});
 	}
 
-	const editUserModal = async () => {
 
-	}
 
 	return (
 		<>
@@ -41,34 +71,111 @@ export const Tbody = ({ usuario, index }) => {
 				<td className="d-flex justify-content-center">
 					<div className="padright deleteuser">
 						<span className="pten" onClick={deleteUser}>
-							<ion-icon name="trash-bin-outline" ></ion-icon>
+							<ion-icon name="trash-bin-outline"></ion-icon>
 						</span>
 					</div>
-					<div className="padright edituser" onClick={editUserModal}>
-						<span className="pten" >
+					<div className="padright edituser">
+						<CButton className="pten" onClick={() => setVisible(!visible)}>
 							<ion-icon name="options-outline"></ion-icon>
-						</span>
+						</CButton>
 					</div>
 				</td>
 			</tr>
-			<Button onClick={handleOpen}>Open modal</Button>
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="parent-modal-title"
-				aria-describedby="parent-modal-description"
-			>
-				<Box sx={{ ...style, width: 400 }}>
-					<h2 id="parent-modal-title">Text in a modal</h2>
-					<p id="parent-modal-description">
-						Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-					</p>
-					<ChildModal />
-				</Box>
-			</Modal>
 
+			<CModal scrollable visible={visible} onClose={() => setVisible(false)}>
+				<CModalHeader>
+					<CModalTitle>Editar usuario</CModalTitle>
+				</CModalHeader>
+				<CModalBody>
+					<section className="container-fluid register-content">
+						<div id="">
+							<div className="row justify-content-center">
+								<div className="col-md-6">
+									<>
+										<FloatingLabel
+											controlId="floatingInputName"
+											label={usuario.nombre}
+											className="mb-3"
+										>
+											<Form.Control
+												type="text"
+												placeholder={usuario.nombre}
+												onChange={(e) => setNombre(e.target.value)}
+											/>
+										</FloatingLabel>
+									</>
+								</div>
+								<div className="col-md-6">
+									<>
+										<FloatingLabel
+											controlId="floatingInputLastName"
+											label="Apellido"
+											className="mb-3"
+										>
+											<Form.Control
+												type="text"
+												placeholder="Ingrese apellido"
+											/>
+										</FloatingLabel>
+									</>
+								</div>
+							</div>
+							<div className="row justify-content-center">
+								<div className="col-md-12">
+									<>
+										<FloatingLabel
+											controlId="floatingInputMail"
+											label="Correo electrónico"
+											className="mb-3"
+										>
+											<Form.Control
+												type="mail"
+												placeholder="Ingrese correo electrónico"
+											/>
+										</FloatingLabel>
+									</>
+								</div>
+							</div>
+							<div className="row justify-content-center">
+								<div className="col-md-6">
+									<FloatingLabel
+										controlId="floatingPassword"
+										label="Contraseña"
+									>
+										<Form.Control
+											type="password"
+											placeholder="Ingrese contraseña"
+										/>
+									</FloatingLabel>
+								</div>
+								<div className="col-md-6">
+									<FloatingLabel
+										controlId="floatingConfirmPassword"
+										label="Confirmar contraseña"
+									>
+										<Form.Control
+											type="password"
+											placeholder="Imgrese contraseña"
+
+										/>
+									</FloatingLabel>
+								</div>
+								<div className="col-md-12 mt-3">
+									<Form.Select aria-label="Tipo">
+										<option disabled selected>
+											SELECCIONAR ROL
+										</option>
+									</Form.Select>
+								</div>
+							</div>
+						</div>
+					</section>
+				</CModalBody>
+				<CModalFooter>
+					<CButton color="secondary" onClick={() => setVisible(false)}>cancelar</CButton>
+					<CButton onClick={submitToggle} className="btn-update">actualizar usuario</CButton>
+				</CModalFooter>
+			</CModal>
 		</>
-	)
-}
-
-
+	);
+};
