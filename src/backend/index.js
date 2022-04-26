68c0205173f6114e166366483d8f0eff25f5ed8c
juanmarcelo5 <marcecinco@htomail.com>
2022-04-25 22:08:58 -0400

    Creacion de la API para US

--- src/backend/index.js
+++ src/backend/index.js
@@ -5,6 +5,8 @@ const usuarioRoute = require('./routes/usuario')
 const authRoute = require('./routes/auth')
 const rolRoute = require('./routes/role')
 const proyectoRoute = require('./routes/proyecto')
+const backlogRoute = require('./routes/backlog')
+const userStoryRoute = require('./routes/userStory')
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -13,6 +15,8 @@ app.use('/api/auth',authRoute)
 app.use('/api/usuario',usuarioRoute)
 app.use('/api/role',rolRoute)
 app.use('/api/proyecto',proyectoRoute)
+app.use('/api/backlog',backlogRoute)
+app.use('/api/user-story',userStoryRoute)
 
 app.listen(4000,()=>{
   console.log('Servidor levantado');
