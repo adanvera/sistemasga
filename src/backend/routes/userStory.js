554e5ae092b128898a4adb08b53e33ae34f120c0
juanmarcelo5 <marcecinco@htomail.com>
2022-05-12 22:22:17 -0400

    Agregando API estado en curso US

--- src/backend/routes/userStory.js
+++ src/backend/routes/userStory.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { agregarUserStory, obtenerUsByBacklog, eliminarUS, actualizarUS } = require('../controllers/userStory')
+const { agregarUserStory, obtenerUsByBacklog, eliminarUS, actualizarUS, obtenerUsByEnCurso } = require('../controllers/userStory')
 const router = express.Router()
 
 router.post('/crear-us',agregarUserStory)
 router.get('/obtener-us-backlog/:id',obtenerUsByBacklog)
+router.get('/obtener-us-en-curso/:id',obtenerUsByEnCurso)
 router.put('/actualizar-us/:id',actualizarUS)
 router.delete('/eliminar-us/:id',eliminarUS)
 
