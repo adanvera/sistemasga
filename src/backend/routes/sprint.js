88513ea20bc1b05aa743f555bd47feebbec87103
juanmarcelo5 <marcecinco@hotmail.com>
2022-06-19 21:26:27 -0400

    feat add new API to get sprint by project ID

--- src/backend/routes/sprint.js
+++ src/backend/routes/sprint.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { crearSprint, obtenerSprint, modificarSprint, agregarUs, obtenerSprintByID, getAllSprint } = require('../controllers/sprint')
+const { crearSprint,modificarSprint, agregarUs, obtenerSprintByID, getAllSprint, sprintByProjectId } = require('../controllers/sprint')
 const router = express.Router()
 /* EndPoint */
 router.post('/crear-sprint',crearSprint)
 router.post('/add-us-sprint/:id',agregarUs)
 router.get('/obtener-sprint/:id',obtenerSprintByID)
+router.get('/sprint-by-project/:id',sprintByProjectId)
 router.get('/obtener-sprint/',getAllSprint)
 router.put('/actualizar-sprint/:id',modificarSprint)
 
